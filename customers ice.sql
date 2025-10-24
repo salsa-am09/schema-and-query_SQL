3257faf35377b1f8c5b4e89e7eea1876fbf08224
salsa-am09 <salsaam09@gmail.com>
2025-10-24 13:07:18 +0700

    Create customers ice.sql
    Created a simple customers table fro fruit ice sales

---
+++ customers ice.sql
@@ -0,0 +1,17 @@
+--- used schema in DB fiddle
+create table customers (
+  id int primary key,
+  customer_name varchar(100),
+  phone_number varchar(50),
+  address varchar(100)
+ );
+ 
+insert into customers (id, customer_name, phone_number, address) values
+(1,'Heni', '08-2145', 'New Karawaci'),
+(2, 'Maya', '08-7356', 'Karawaci'),
+(3, 'Levin', '08-9651', 'Karawaci'),
+(4, 'June', '08-7800', 'Senayan'),
+(5, 'Lia', '08-2459', 'Kavling');
+
+--- the link below
+[View on DB Fiddle](https://www.db-fiddle.com/f/3XCrRavwNy8aeSDdXtVpP6/0)
